29bb5490f1dfafd517018fe9f0654fdddc770ea7
mindotori <148763440+mindotori@users.noreply.github.com>
2024-09-09 03:25:17 +0900

    Backjoon:1998년생인 내가 태국에서는 2541년생?!

---
+++ src/calculation/Thailand.java
@@ -0,0 +1,13 @@
+package calculation;
+
+import java.util.Scanner;
+
+public class Thailand {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+
+        System.out.println(y-543);
+    }
+
+}
