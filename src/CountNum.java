47f5fefc95d4951cd64a156ae4587d93f4738325
mindotori <148763440+mindotori@users.noreply.github.com>
2024-09-28 18:48:45 +0900

    BackJoon: 단어의 개수

---
+++ src/CountNum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CountNum {
+    public static void main(String[] args) {
+        //공백이 연속해서 나오는 경우는 없다. 문자열은 공백으로 시작하거나 끝날 수 있다.*****
+        Scanner sc =  new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        if(!trim)
+    }
+}
