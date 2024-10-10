913ce1965bbec08074421c27fbd7fd1368da3ca6
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-10 19:24:57 +0900

    백준: 2743 단어 길이 재기

---
+++ src/string/LengthOfStr.java
@@ -0,0 +1,11 @@
+package string;
+
+import java.util.Scanner;
+
+public class LengthOfStr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.length());
+    }
+}
