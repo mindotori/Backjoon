ecc4adedfeb005b873b4f3a168d8c7c4ef40aeee
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-13 17:16:47 +0900

    백준: 9086 문자열

---
+++ src/string/String9086.java
@@ -0,0 +1,24 @@
+package string;
+
+import java.util.Scanner;
+
+public class String9086 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+            String answer = "";
+
+            if (str.length() == 1){ //문자열 길이가 1인 경우
+                answer = str + str;
+            } else{
+                answer = str.charAt(0) + "" + str.charAt(str.length() - 1);
+            }
+            System.out.println(answer);
+        }
+    }
+}
