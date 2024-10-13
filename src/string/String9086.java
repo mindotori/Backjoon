921e546050f1c13a348ae2c976769a71d6f30af3
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-13 17:27:16 +0900

    백준: 9086 문자열 수정

--- src/string/String9086.java
+++ src/string/String9086.java
@@ -7,7 +7,7 @@ public class String9086 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine();
+        sc.nextLine(); //입력 버퍼 정리
 
         for (int i = 0; i < n; i++) {
             String str = sc.nextLine();
