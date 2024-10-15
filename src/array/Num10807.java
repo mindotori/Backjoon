8bbc4151e2e9d9297e829d6c753c4081f644fa09
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-15 20:02:24 +0900

    백준: 10807개수 세기

---
+++ src/array/Num10807.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Num10807 {
+    public static void main(String[] args) {
+        Scanner sc=  new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int n2 = sc.nextInt();
+            arr[i] += n2;
+        }
+        int n3 = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(n3==arr[i]){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
