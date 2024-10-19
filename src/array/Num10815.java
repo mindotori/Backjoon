3bd6a244cb3fb635a08ee3bad3cfa691a37186fc
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-19 15:22:09 +0900

    백준 10815 숫자 카드

--- src/array/Num10815.java
+++ src/array/Num10815.java
@@ -9,14 +9,16 @@ public class Num10815 {
         int n1 = sc.nextInt();
         int[] arr1 = new int[n1];
         for (int i = 0; i < n1; i++) {
-            int n2 = sc.nextInt();
-            arr1[i] += n2;
+//            int n2 = sc.nextInt();
+//            arr1[i] += n2;
+            arr1[i] = sc.nextInt();
         }
         int n3 = sc.nextInt();
         int[] arr2 = new int[n3];
         for (int i = 0; i < n3; i++) {
-            int n4 = sc.nextInt();
-            arr2[i] += n4;
+//            int n4 = sc.nextInt();
+//            arr2[i] += n4;
+            arr2[i] = sc.nextInt()
         }
 
         //출력값 담을 배열
