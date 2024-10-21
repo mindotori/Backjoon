931667d7d9cb9a452d8df8c7c3583feb95c446de
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-21 22:57:33 +0900

    백준 3052 나머지

---
+++ src/array/Num3052.java
@@ -0,0 +1,23 @@
+package array;
+
+import java.util.*;
+
+public class Num3052 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        List<Integer> remain = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(sc.nextInt());
+        }
+
+        // 각 원소들의 42로 나눈 나머지 배열
+        for (int i = 0; i < list.size(); i++) {
+            remain.add(list.get(i)%42);
+        }
+        // Set을 사용하여 중복값을 하나로
+        Set<Integer> unique = new HashSet<>(remain);
+        System.out.println(unique.size()); // 그냥 배열 크기 반환
+}
+}
