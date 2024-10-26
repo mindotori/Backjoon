f6a31c9fc285cf89ca31ba13bece6022676bc230
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-26 19:52:47 +0900

    백준 11382 꼬마 정민

---
+++ src/math/Num11382.java
@@ -0,0 +1,15 @@
+package math;
+import java.util.*;
+
+public class Num11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 1012)이 공백을 사이에 두고 주어진다.
+        //위의 문장이 핵심 => Long을 썼어야 함
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a + b + c);
+    }
+    }
