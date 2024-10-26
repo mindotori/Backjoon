ff6a03812230c5182d63786dfde087fd70a2500a
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-26 20:26:28 +0900

    백준 10171 고양이

---
+++ src/string/Num10171.java
@@ -0,0 +1,11 @@
+package string;
+
+public class Num10171 {
+//이스케이프 시퀀스
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
