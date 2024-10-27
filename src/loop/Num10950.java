969a5530f91b3a501baaa724ef7fb4f7ce08942a
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-27 17:53:46 +0900

    백준 8393 합

---
+++ src/loop/Num10950.java
@@ -0,0 +1,19 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Num10950 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a+b);
+        }
+    }
+}
