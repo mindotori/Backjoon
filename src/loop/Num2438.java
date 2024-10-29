e789f8968164b70150e1de693f3517664e769400
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-29 15:40:23 +0900

    백준 2439 별 찍기 -1

---
+++ src/loop/Num2438.java
@@ -0,0 +1,16 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Num2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String star = "*";
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(star.repeat(i));
+        }
+    }
+}
