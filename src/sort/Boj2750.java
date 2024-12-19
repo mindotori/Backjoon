d6fb8f07f1f150b52d654b6263fbf7468c57a0aa
mindotori <148763440+mindotori@users.noreply.github.com>
2024-12-19 20:51:44 +0900

    백준10989 수 정렬하기3

--- src/sort/Boj2750.java
+++ src/sort/Boj2750.java
@@ -1,13 +1,14 @@
 package sort;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
-public class Boj10989 {
+public class Boj2750 {
     public static void main(String[] args) throws IOException {
+        // scanner대신 br로 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
@@ -15,14 +16,11 @@ public class Boj10989 {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
+
         Arrays.sort(arr);
 
-        //BufferedWriter 사용하여 출력
         for (int i : arr) {
-            bw.write(i + "\n");
+            System.out.println(i);
         }
-
-        bw.flush();
-        bw.close();
     }
 }
